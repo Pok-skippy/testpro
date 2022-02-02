f21801529daf14aba5a6aff9047d29bd95ed31ab
Pok-skippy <pokskippy@hotmail.com>
2022-02-02 19:06:30 +0100

    login

---
+++ src/client/Angularclient/src/app/pages/idcom/idcomlogin.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbDatepickerModule, NbIconModule,
+  NbInputModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbUserModule,
+} from '@nebular/theme';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { PageLoginComponent } from './pagelogin/idcompagelogin.component';
+import { FormsModule as ngFormsModule } from '@angular/forms';
+import { IdcomLoginRoutingModule } from './idcomlogin-routing.module';
+import { IdcomLoginComponent } from './idcomlogin.component';
+
+@NgModule({
+  imports: [
+    ThemeModule,
+    NbInputModule,
+    NbCardModule,
+    NbButtonModule,
+    NbActionsModule,
+    NbUserModule,
+    NbCheckboxModule,
+    NbRadioModule,
+    NbDatepickerModule,
+    IdcomLoginRoutingModule,
+    NbSelectModule,
+    NbIconModule,
+    ngFormsModule,
+  ],
+  declarations: [
+    IdcomLoginComponent,
+    PageLoginComponent,
+  ],
+})
+export class IdcomLoginModule { }
